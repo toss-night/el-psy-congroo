d0960d699f9c03973d795c8604b5ed82527006bd
toss-night <areskindenis0@gmail.com>
2025-10-26 20:45:52 +0300

    Добавил геттеры, сеттеры, конструкторы и проверку данных в класс "Студент"

--- Student_accounting_system/Student_accounting_system.cpp
+++ Student_accounting_system/Student_accounting_system.cpp
@@ -6,10 +6,65 @@ using namespace std;
 
 class Student
 {
-public:
+private:
     string name;
     int age;
     vector <int>grades;
+
+public:
+
+    Student() {}
+    Student(string nname, int nage, vector<int>ngrades)
+    {
+        if (!nname.empty())
+        {
+            name = nname;
+        }
+        if (nage >= 16 && nage <= 60)
+        {
+            age = nage;
+        }
+        bool grdsValid = false;
+        for (int grade : ngrades)
+        {
+            if (grade <= 5 && grade >= 1)
+            {
+                grdsValid = true;
+            }
+        }
+        if (grdsValid)
+        {
+            grades = ngrades;
+        }
+    }
+
+    void setName(string nname)
+    {
+        if (!nname.empty())
+        {
+            name = nname;
+        }
+    }
+    void setAge(int nage)
+    {
+        if (nage >= 16 && nage <= 60)
+        {
+            age = nage;
+        }
+    }
+    
+    string getName()
+    {
+        return name;
+    }
+    int getAge()
+    {
+        return age;
+    }
+    vector<int> getGrades()
+    {
+        return grades;
+    }
 
     double calculateAverage()
     {
@@ -18,7 +73,14 @@ public:
         {
             sumgrd += grd;
         }
-        return sumgrd / grades.size();
+        if (grades.size() != 0)
+        {
+            return sumgrd / grades.size();
+        }
+        else
+        {
+            return 0.0;
+        }
     }
 
     void addGrade(int grade)
@@ -47,6 +109,7 @@ public:
 
 int main()
 {
+    setlocale(LC_ALL, "ru");
     
 }
 
