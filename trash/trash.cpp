905ba1d704e0f9c637ef09f173c0364498a0a242
toss-night <areskindenis0@gmail.com>
2025-11-06 21:40:57 +0300

    развлекался с простыми указателями

---
+++ trash/trash.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+void swapValues(int* a, int* b) 
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void swapValuesRef(int& a, int& b) 
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+class Point 
+{
+public:
+    int x, y;
+    Point() {}
+    Point(int a, int b) :x(a), y(b) {}
+};
+
+
+Point* createPoint(int x, int y) 
+{
+    Point *z = new Point(x, y);
+    return z;
+}
+
+void doubleArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        *(arr+i) *= 2;
+    }
+}
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    
+}
+
