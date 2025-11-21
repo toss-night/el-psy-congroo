de61103251acaa0f454183ce4816f1dee7c23d2e
toss-night <areskindenis0@gmail.com>
2025-11-21 23:37:23 +0300

    Implemented a unique_ptr

--- smart-pointer/smart-pointer.cpp
+++ smart-pointer/smart-pointer.cpp
@@ -10,17 +10,35 @@ private:
 
 public:    
     explicit unique_ptr(T* obj=nullptr) :ptr(obj) {}
+
     ~unique_ptr() { delete ptr; }
+
     unique_ptr(const unique_ptr&) = delete;
     unique_ptr& operator = (const unique_ptr&) = delete;
     
+    unique_ptr(unique_ptr&& other) noexcept : ptr(other.ptr) { other.ptr = nullptr; }
+
+    T* get() const { return ptr; }
+
+    T* release()
+    {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    void reset(T* new_ptr=nullptr)
+    {
+        delete ptr;
+        ptr = new_ptr;
+    }
 };
 
 
 
 
 int main()
-{
-    
+{    
+
 }
 
