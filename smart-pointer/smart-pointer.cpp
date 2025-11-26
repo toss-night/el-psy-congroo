350390a7fe0d1518763987ce6a2491fbea897fba
toss-night <areskindenis0@gmail.com>
2025-11-26 12:38:19 +0300

    implemented weak_ptr and fixed several bugs in shared_ptr

--- smart-pointer/smart-pointer.cpp
+++ smart-pointer/smart-pointer.cpp
@@ -1,7 +1,5 @@
 ﻿#include <iostream>
 
-
-
 template<typename T>
 class unique_ptr
 {
@@ -61,6 +59,14 @@ private:
 public:
     explicit shared_ptr(T* obj = nullptr) : ptr(obj), count(obj ? new int(1) : nullptr) {}
 
+    shared_ptr(T* p, int* cnt) : ptr(p), count(cnt) 
+    {
+        if (count) 
+        {
+            (*count)++;  
+        }
+    }
+
     shared_ptr(const shared_ptr& other) :ptr(other.ptr), count(other.count) { addRef(); }
     shared_ptr& operator= (const shared_ptr& other)
     {
@@ -82,11 +88,65 @@ public:
     T& operator*() const { return *ptr; }
     T* operator->() const { return ptr; }
     explicit operator bool() const { return ptr != nullptr; }
+
+    template<typename U>
+    friend class weak_ptr;
 };
 
+template<typename T>
+class weak_ptr
+{
+private:
+    T* ptr;
+    int* count;
+
+public:
+    weak_ptr() :ptr(nullptr), count(nullptr) {}
+    
+    weak_ptr(const shared_ptr<T>& other) :ptr(other.ptr), count(other.count) {}
+
+    weak_ptr(const weak_ptr& other) :ptr(other.ptr), count(other.count) {}
+
+    weak_ptr(weak_ptr&& other) noexcept :ptr(other.ptr), count(other.count) 
+    {
+        other.ptr = nullptr;
+        other.count = nullptr;
+    } 
+    
+    weak_ptr& operator=(weak_ptr&& other) noexcept 
+    {
+        if (this != &other) 
+        {
+            ptr = other.ptr;
+            count = other.count;
+            other.ptr = nullptr;
+            other.count = nullptr;
+        }
+        return *this;
+    }
+    
+    bool expired() const { return !count || *count == 0; };
+ 
+    shared_ptr<T> lock() const
+    {
+        if (count && *count > 0)
+        {
+            return shared_ptr<T>(ptr, count);
+        }
+        return shared_ptr<T>();
+    }
+
+    int use_count() const { return count ? *count : 0; }
+        
+    void reset() 
+    {
+        ptr = nullptr;
+        count = nullptr;
+    }
+};
 
 int main()
 {    
-   
+    return 0;
 }
 
